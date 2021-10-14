eaffdd089fbf54faff8ae024ed3f0b6f81b78adb
Beckett Jacobs <beckettjaconsgomputerscience@gmail.com>
2021-10-14 09:55:37 -0700

    PIN

---
+++ crontrol flow.py
@@ -0,0 +1,21 @@
+# Programmer: Beckett Jacobs
+# Date: 10.11.2021
+# Program: ATM Bank Transaction
+
+"""
+This program simulates an ATM If, Elif, & Else statements
+Nesting If statements and refresh our Comparison & Logical Operators
+"""
+
+print("Welcome to Cash-R-Us Bank\nLet's take a moment to set up your account.\n")
+
+# set up account by asking users for their first & Last names using Variables
+first_name = input("What is your first name: ")
+last_name = input("What is your last name: ")
+
+print("\nWelcome to Cash-R-Us",first_name,last_name +",we will set up a escurity PIN on your account.\n")
+
+# set up a PIN - Personal Identification Number
+pin = input("please choose a 4 digit Personal Identification Number: ")
+
+print("\nThank you",first_name + ", we see that you set your PIN to",pin)
