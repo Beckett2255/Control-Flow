f05103d27d811976a51cfbe19eb0532cd6d0c34c
Beckett Jacobs <beckettjacobsgomputerscience@gmial.com>
2021-10-25 12:49:36 -0400

    add ATM variable and if and statements

--- crontrol flow.py
+++ crontrol flow.py
@@ -13,9 +13,21 @@ print("Welcome to Cash-R-Us Bank\nLet's take a moment to set up your account.\n"
 first_name = input("What is your first name: ")
 last_name = input("What is your last name: ")
 
-print("\nWelcome to Cash-R-Us",first_name,last_name +",we will set up a escurity PIN on your account.\n")
+print("\nWelcome to Cash-R-Us",first_name,last_name +", we will now set up a escurity PIN on your account.\n")
 
 # set up a PIN - Personal Identification Number
 pin = input("please choose a 4 digit Personal Identification Number: ")
 
 print("\nThank you",first_name + ", we see that you set your PIN to",pin)
+
+print("\nWould you like to make a transaction through our Automated Teller Machine")
+atm = input("Yes or No: ").lower()
+
+if atm == "yes":
+    print("\n********************************************\n")
+
+
+
+
+else:
+    print("\nHave a wonderful day", first_name,last_name,", please come back and visit soon.")
